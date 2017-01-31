12383867c4720f3ae8e3009fdb134918b07aaab1
James Falkner <schtool@gmail.com>
2017-01-31 17:51:36 -0500

    Update PingServlet2MDBTopic.java

--- javaee6/modules/web/src/main/java/org/apache/geronimo/daytrader/javaee6/web/prims/ejb3/PingServlet2MDBTopic.java
+++ javaee6/modules/web/src/main/java/org/apache/geronimo/daytrader/javaee6/web/prims/ejb3/PingServlet2MDBTopic.java
@@ -52,7 +52,7 @@ public class PingServlet2MDBTopic extends HttpServlet {
 
     private static int hitCount;
 
-    @Resource(name = "jms/TopicConnectionFactory")
+    @Resource(name = "/ConnectionFactory")
     private ConnectionFactory topicConnectionFactory;
 
     @Resource(name = "jms/TradeStreamerTopic")
